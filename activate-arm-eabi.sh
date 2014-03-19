eb6f003bb92e3e979edff461667c2b2c92c4ba20
Lain-dono <lain.dono@gmail.com>
2014-03-19 11:47:50 +0400

    Добавил "активатор" для тулчейна

---
+++ activate-arm-eabi.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export TARGET=arm
+export TOOLPREFIX=arm-eabi
+
+TOOLCHAIN_PATH="`pwd`/toolchain/$TARGET/bin"
+
+if [ ! -d $TOOLCHAIN_PATH ]; then 
+	pushd toolchain
+		ARCHES=arm ./toolchain.sh
+	popd
+fi
+
+export PATH="$TOOLCHAIN_PATH:$PATH"
+export QEMU=qemu-system-$TARGET
+
